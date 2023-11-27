cf84948e132fa3e9ec2c6fb21c146b068f8c7d6f
Amber <anborong1314@gmail.com>
2023-11-27 14:54:15 +0800

    update my diary script

--- src/diary.ts
+++ src/diary.ts
@@ -2,12 +2,13 @@
 
 import dayjs from "dayjs";
 import inquirer, { QuestionCollection } from "inquirer";
-import { confirm } from "@inquirer/prompts";
+import { confirm, input, editor } from "@inquirer/prompts";
 import { exec } from "child_process";
-import { writeFile } from "fs/promises";
+import { existsSync } from "fs";
+import { readFile, writeFile } from "fs/promises";
 
 const BLOG_ROOT = `${process.env.HOME}/code/diary`;
-const BLOG_DIR = "/blog";
+const BLOG_DIR = "blog";
 
 interface Diary {
   body: string;
@@ -46,23 +47,40 @@ ${body}
 
 async function main() {
   const day = dayjs();
+  const title = day.format("YYYY-MM-DD");
+  const path = `${BLOG_ROOT}/${BLOG_DIR}/${title}.md`;
+  let body = "";
+  let state = false;
+  try {
+    const d = (await readFile(path)).toString();
+    if (d) {
+      const strs = d.split("\n");
+      body = strs.slice(6, strs.length).join("\n");
+      state = true;
+    }
+  } catch (e) {
+    console.error(e);
+  }
 
-  const answer = await inquirer.prompt(questions);
-  const str = genDiaryString({
-    body: answer.body,
-  });
-  console.log(str);
-  const c = await confirm({ message: "isOK?" });
-  if (!c) return;
+  let flag = false;
+
+  while (!flag) {
+    body = await editor({ default: body, message: "body?" });
+    console.log(genDiaryString({ body }));
+    flag = await confirm({ message: "isOk?", default: false });
+  }
+
   // 清空git
   await runCmd(`cd ${BLOG_ROOT} && git pull`);
-  // 创建文件
+
   await writeFile(
     `${BLOG_ROOT}/${BLOG_DIR}/${day.format("YYYY-MM-DD")}.md`,
-    str
+    genDiaryString({ body })
   );
   await runCmd(
-    `cd ${BLOG_ROOT} && git add . && git commit -m "publish a diary" && git push`
+    `cd ${BLOG_ROOT} && git add . && git commit -m "${
+      !state ? "add" : "modify"
+    } ${title}" && git push`
   );
 }
 
